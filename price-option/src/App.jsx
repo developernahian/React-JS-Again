6ad37ff0e94825e54790de65ff8e5999fc36d8e6
developernahian <developer.nahian@gmail.com>
2024-10-29 07:28:54 +0600

    first commit

---
+++ price-option/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+
+  return (
+    <>
+
+      
+
+      <DaisyNav />
+
+    </>
+  )
+}
+
+export default App
